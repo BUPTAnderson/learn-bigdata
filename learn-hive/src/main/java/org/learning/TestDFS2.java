26058dcbefbdd1a3f8e354ca6ee1488d3f5a9662
BUPTAnderson <229546106@qq.com>
2017-08-21 10:35:32 +0800

    add learn-flume module

--- learn-hive/src/main/java/org/learning/TestDFS2.java
+++ learn-hive/src/main/java/org/learning/TestDFS2.java
@@ -20,8 +20,11 @@ public class TestDFS2
     {
         String command = "select * from default.partition_test limit 10";
         command = "INSERT INTO TABLE test.access_log_temp2 SELECT a.users, a.prono, p.maker, p.price FROM access_log_hbase a JOIN product_hbase p ON (a.prono = p.prono)";
-        command = "select id from (select id,devid from tb_in_base) a";
-        command = "INSERT OVERWRITE TABLE access_log_temp2 SELECT a.users, a.prono, p.maker, p.price FROM access_log_hbase a JOIN product_hbase p ON (a.prono = p.prono)";
+        command = "select a.member_id from (select member_id,name from partition_test) a";
+        command = "select a.member_id b from (select member_id,name from partition_test) a";
+//        command = "INSERT OVERWRITE TABLE access_log_temp2 SELECT a.users, a.prono, p.maker, p.price FROM access_log_hbase a JOIN product_hbase p ON (a.prono = p.prono)";
+//        command = "insert overwrite table partition_test partition(stat_date='20110728',province='henan') select member_id,name from partition_test_input where stat_date='20110728' and province='henan'";
+//        command = "insert overwrite table partition_test partition(stat_date='20110728',province='henan') if not exists select member_id,name from partition_test_input where stat_date='20110728' and province='henan'";
         HiveConf conf = new HiveConf(SessionState.class);
         conf.set("_hive.hdfs.session.path", "/tmp");
         conf.set("hive.metastore.uris", "thrift://bds-test-003:9083");
@@ -60,6 +63,11 @@ public class TestDFS2
 //        System.out.println(tableIdName);
 //        String alias = SemanticAnalyzer.getUnescapedUnqualifiedTableName(tree);
 //        System.out.println(alias);
+        String[] strs = new String[3];
+        strs[0] = "abc";
+        strs[1] = "def";
+        strs[2] = "ghi";
+        System.out.println(strs);
     }
 }
 
@@ -80,6 +88,40 @@ public class TestDFS2
  ---|---|---|---|TOK_ALLCOLREF(646)
  ---|---|TOK_LIMIT(797)
  ---|---|---|10(321)
+ */
+
+/**
+ select a.member_id from (select member_id,name from partition_test) a
+ TOK_QUERY(860)
+ ---|TOK_FROM(748)
+ ---|---|TOK_SUBQUERY(919)
+ ---|---|---|TOK_QUERY(860)
+ ---|---|---|---|TOK_FROM(748)
+ ---|---|---|---|---|TOK_TABREF(954)
+ ---|---|---|---|---|---|TOK_TABNAME(953)
+ ---|---|---|---|---|---|---|partition_test(26)
+ ---|---|---|---|TOK_INSERT(772)
+ ---|---|---|---|---|TOK_DESTINATION(726)
+ ---|---|---|---|---|---|TOK_DIR(727)
+ ---|---|---|---|---|---|---|TOK_TMP_FILE(963)
+ ---|---|---|---|---|TOK_SELECT(878)
+ ---|---|---|---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|---|---|member_id(26)
+ ---|---|---|---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|---|---|name(26)
+ ---|---|---|a(26)
+ ---|TOK_INSERT(772)
+ ---|---|TOK_DESTINATION(726)
+ ---|---|---|TOK_DIR(727)
+ ---|---|---|---|TOK_TMP_FILE(963)
+ ---|---|TOK_SELECT(878)
+ ---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|.(17)
+ ---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|a(26)
+ ---|---|---|---|---|member_id(26)
  */
 
 /**
@@ -134,38 +176,6 @@ public class TestDFS2
  */
 
 /**
- select id from (select id,devid from tb_in_base) a
- TOK_QUERY(860)
- ---|TOK_FROM(748)
- ---|---|TOK_SUBQUERY(919)
- ---|---|---|TOK_QUERY(860)
- ---|---|---|---|TOK_FROM(748)
- ---|---|---|---|---|TOK_TABREF(954)
- ---|---|---|---|---|---|TOK_TABNAME(953)
- ---|---|---|---|---|---|---|tb_in_base(26)
- ---|---|---|---|TOK_INSERT(772)
- ---|---|---|---|---|TOK_DESTINATION(726)
- ---|---|---|---|---|---|TOK_DIR(727)
- ---|---|---|---|---|---|---|TOK_TMP_FILE(963)
- ---|---|---|---|---|TOK_SELECT(878)
- ---|---|---|---|---|---|TOK_SELEXPR(880)
- ---|---|---|---|---|---|---|TOK_TABLE_OR_COL(950)
- ---|---|---|---|---|---|---|---|id(26)
- ---|---|---|---|---|---|TOK_SELEXPR(880)
- ---|---|---|---|---|---|---|TOK_TABLE_OR_COL(950)
- ---|---|---|---|---|---|---|---|devid(26)
- ---|---|---|a(26)
- ---|TOK_INSERT(772)
- ---|---|TOK_DESTINATION(726)
- ---|---|---|TOK_DIR(727)
- ---|---|---|---|TOK_TMP_FILE(963)
- ---|---|TOK_SELECT(878)
- ---|---|---|TOK_SELEXPR(880)
- ---|---|---|---|TOK_TABLE_OR_COL(950)
- ---|---|---|---|---|id(26)
- */
-
-/**
  INSERT OVERWRITE TABLE access_log_temp2 SELECT a.users, a.prono, p.maker, p.price FROM access_log_hbase a JOIN product_hbase p ON (a.prono = p.prono)
  TOK_QUERY(860)
  ---|TOK_FROM(748)
@@ -213,4 +223,83 @@ public class TestDFS2
  ---|---|---|---|---|TOK_TABLE_OR_COL(950)
  ---|---|---|---|---|---|p(26)
  ---|---|---|---|---|price(26)
+ */
+
+/**
+ insert overwrite table partition_test partition(stat_date='20110728',province='henan') select member_id,name from partition_test_input where stat_date='20110728' and province='henan'
+
+ TOK_QUERY(860)
+ ---|TOK_FROM(748)
+ ---|---|TOK_TABREF(954)
+ ---|---|---|TOK_TABNAME(953)
+ ---|---|---|---|partition_test_input(26)
+ ---|TOK_INSERT(772)
+ ---|---|TOK_DESTINATION(726)
+ ---|---|---|TOK_TAB(925)
+ ---|---|---|---|TOK_TABNAME(953)
+ ---|---|---|---|---|partition_test(26)
+ ---|---|---|---|TOK_PARTSPEC(839)
+ ---|---|---|---|---|TOK_PARTVAL(840)
+ ---|---|---|---|---|---|stat_date(26)
+ ---|---|---|---|---|---|'20110728'(332)
+ ---|---|---|---|---|TOK_PARTVAL(840)
+ ---|---|---|---|---|---|province(26)
+ ---|---|---|---|---|---|'henan'(332)
+ ---|---|TOK_SELECT(878)
+ ---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|member_id(26)
+ ---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|name(26)
+ ---|---|TOK_WHERE(988)
+ ---|---|---|and(34)
+ ---|---|---|---|=(20)
+ ---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|stat_date(26)
+ ---|---|---|---|---|'20110728'(332)
+ ---|---|---|---|=(20)
+ ---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|province(26)
+ ---|---|---|---|---|'henan'(332)
+ */
+
+/**
+ insert overwrite table partition_test partition(stat_date='20110728',province='henan') if not exists select member_id,name from partition_test_input where stat_date='20110728' and province='henan'
+
+ TOK_QUERY(860)
+ ---|TOK_FROM(748)
+ ---|---|TOK_TABREF(954)
+ ---|---|---|TOK_TABNAME(953)
+ ---|---|---|---|partition_test_input(26)
+ ---|TOK_INSERT(772)
+ ---|---|TOK_DESTINATION(726)
+ ---|---|---|TOK_TAB(925)
+ ---|---|---|---|TOK_TABNAME(953)
+ ---|---|---|---|---|partition_test(26)
+ ---|---|---|---|TOK_PARTSPEC(839)
+ ---|---|---|---|---|TOK_PARTVAL(840)
+ ---|---|---|---|---|---|stat_date(26)
+ ---|---|---|---|---|---|'20110728'(332)
+ ---|---|---|---|---|TOK_PARTVAL(840)
+ ---|---|---|---|---|---|province(26)
+ ---|---|---|---|---|---|'henan'(332)
+ ---|---|---|TOK_IFNOTEXISTS(767)
+ ---|---|TOK_SELECT(878)
+ ---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|member_id(26)
+ ---|---|---|TOK_SELEXPR(880)
+ ---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|name(26)
+ ---|---|TOK_WHERE(988)
+ ---|---|---|and(34)
+ ---|---|---|---|=(20)
+ ---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|stat_date(26)
+ ---|---|---|---|---|'20110728'(332)
+ ---|---|---|---|=(20)
+ ---|---|---|---|---|TOK_TABLE_OR_COL(950)
+ ---|---|---|---|---|---|province(26)
+ ---|---|---|---|---|'henan'(332)
  */
