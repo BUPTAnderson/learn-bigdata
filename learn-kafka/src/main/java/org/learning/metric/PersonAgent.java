78bb87a9cd04b350ab46c0f67787bc048857844b
BUPTAnderson <229546106@qq.com>
2017-11-21 10:50:54 +0800

    add kafka metrics demo

---
+++ learn-kafka/src/main/java/org/learning/metric/PersonAgent.java
@@ -0,0 +1,27 @@
+package org.learning.metric;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * Created by anderson on 17-11-20.
+ */
+public class PersonAgent
+{
+    public static void main(String[] args)
+            throws MalformedObjectNameException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException, InterruptedException
+    {
+        // 获取MBeanServer对象
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        ObjectName personName = new ObjectName("jmxBean:name=xiaoming");
+//        server.registerMBean(new Person("xiaoming", 27), personName);
+        server.registerMBean(new PersonDynamic(new Person("xiaoming", 27)), personName);
+        Thread.sleep(60 * 60 *1000);
+    }
+}
---
+++ learn-kafka/src/main/java/org/learning/metric/PersonMBean.java
@@ -0,0 +1,11 @@
+package org.learning.metric;
+
+/**
+ * Created by anderson on 17-11-20.
+ */
+public interface PersonMBean
+{
+    String getName();
+    int getAge();
+    String sayHello(String hello);
+}
