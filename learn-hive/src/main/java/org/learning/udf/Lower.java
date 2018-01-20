936bc7d3f962d4e092838b2c73aa10e68e128fde
BUPTAnderson <229546106@qq.com>
2018-01-20 13:28:02 +0800

    update module

--- learn-hive/src/main/java/org/learning/udf/Lower.java
+++ learn-hive/src/main/java/org/learning/udf/Lower.java
@@ -4,9 +4,22 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 
 /**
+ * 一般运行包含manifest的jar包，可以使用
+
+ * java -jar <jar-file-name>.jar
+ * 如果jar里没有 manifest，则可以使用
+
+ * java -cp foo.jar full.package.name.ClassName 或者: java -classpath foo.jar full.package.name.ClassName
+ * 当main类依赖多个jar时，可以把多个jar打包到一个目录，然后用-Djava.ext.dirs指定该目录，引用依赖的多个jar。
+
+ * java -Djava.ext.dirs=<多个jar包的目录> com.test.HelloWordMain
+ * 如果用-cp则需要写每一个jar，很麻烦
+ */
+
+/**
  * 编译时指定依赖的jar包
  * javac -classpath /home/hadoop/redistest/jedis-2.8.2.jar RedisJava.java
- * 执行时指定jar所在目录 java -Djava.ext.dirs=/home/hadoop/redistest -cp . RedisJava
+ * 执行时指定依赖jar所在目录 java -Djava.ext.dirs=/home/hadoop/redistest -cp . RedisJava
 
  * Created by anderson on 17-10-13.
  * 编写文件:
